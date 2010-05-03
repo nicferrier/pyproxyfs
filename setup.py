0ba310603807fa197578267ae66b3d1f1b536a46
Nic Ferrier <nferrier@ferrier.me.uk>
2010-05-03 04:23:21 +0100

    distribution metafiles

---
+++ setup.py
@@ -0,0 +1,29 @@
+#from setuptools import setup
+#from setuptools import find_packages
+from distutils.core import setup
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Python Software Foundation License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: System :: Filesystems',
+]
+
+setup(
+    name = "PyProxyFS",
+    version = "0.1",
+    description = "Simple filesystem abstraction",
+    long_description = """A proxy filesystem interface with a native
+filesystem implementation and a very simple test in-memory filesystem.""",
+    license = "GNU GPL v3",
+    author = "Nic Ferrier",
+    author_email = "nic@ferrier.me.uk",
+    url = "http://github.com/nicferrier/pyproxyfs",
+    download_url="",
+    platforms = ["any"],
+    packages = ["pyproxyfs"],
+    package_dir = {"":"src"},
+    classifiers =  classifiers
+    )
